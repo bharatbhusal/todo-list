42b1d96b43011af81ca533faa58f09a9624147af
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-12 22:26:03 +0530

    adding count

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -17,6 +17,7 @@ export default function TodoList({ todos, setTodos }) {
                 {todos.map(todo => {
                     return <TodoItem {...todo} key={todo.id} setTodos={setTodos} />;
                 })}
+                {todos.length!==0 && <h4>Total Todos: {todos.length}</h4>}
             </ul>
         </>
     );
